e79cc2981eefa1b1849aac68f34b730b4d209f8e
Fran <franbl98@gmail.com>
2022-01-12 15:35:22 +0100

    Fin parte 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,19 +11,10 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'users', component: UsersComponent, children: [
-    { path: ':id/:name', component: UserComponent }]},
-  { path: 'servers', component: ServersComponent, children: [
-    { path: ':id/edit', component: EditServerComponent },
-    { path: ':id', component: ServerComponent }]},
-    { path: 'not-found', component: PageNotFoundComponent},
-    { path: '**', redirectTo: '/not-found'}
-];
+import { AppRoutingModule } from './app-routing.module';
+import { AuthGuard } from './AuthGuard.service';
+import { AuthService } from './auth.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -38,9 +29,9 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
-  providers: [ServersService],
+  providers: [ServersService, AuthGuard, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
+import { Observable } from "rxjs";
+ 
+@Injectable()
+export class AuthService {
+ 
+  loggedIn = false;
+ 
+
+
+  isAuthenticated() {
+    const promise = new Promise(
+      (resolve, reject) => {
+        setTimeout(() => {
+          resolve(this.loggedIn);
+        }, 800);
+      }
+    );
+    return promise;
+  }
+ 
+  login() {
+    this.loggedIn = true;
+  }
+ 
+  logout() {
+    this.loggedIn = false;
+  }
+}
