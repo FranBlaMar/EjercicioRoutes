87214aae6c5074a6e3f1a595290df31d90d7539e
Fran <franbl98@gmail.com>
2022-01-12 16:25:30 +0100

    Fin parte 7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AuthGuard } from "./AuthGuard.service";
+import { ErrorPageComponent } from "./error-page/error-page.component";
 import { HomeComponent } from "./home/home.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { ServerResolver } from "./servers/server/server-resolver.service";
 import { ServerComponent } from "./servers/server/server.component";
 import { ServersComponent } from "./servers/servers.component";
 import { UserComponent } from "./users/user/user.component";
@@ -16,11 +19,11 @@ const appRoutes: Routes = [
     },
     { path: '', component: HomeComponent },
     { path: 'servers', canActivateChild: [AuthGuard], component: ServersComponent, children: [
-        { path: ':id/edit', component: EditServerComponent },
+        { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard],resolve: { server: ServerResolver }  },
         { path: ':id', component: ServerComponent }
     ] 
     },
-    { path: 'not-found', component: PageNotFoundComponent},
+    { path: 'not-found', component: ErrorPageComponent, data: {message: 'Ooopsi! Page not found.'} },
     { path: '**', redirectTo: '/not-found'} 
 ];
  
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router} from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,11 +9,18 @@ import { Router} from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
   }
   onLoadServer(numServer: number) {
     this.router.navigate(['/servers', numServer, 'edit'], { queryParams: { allowEdit: '8' }, fragment: 'loading' });
+  }
+  onlogin() {
+    this.authService.login();
+  }
+ 
+  onlogout() {
+    this.authService.logout();
   }
 }
